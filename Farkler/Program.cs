d015517f17cbe0aa522526ad13897c833ac64a4e
juanbond <archaeal@gmail.com>
2015-02-26 01:37:27 -0600

    ev work

---
+++ Farkler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farkler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program instance = new Program();
+            instance.go();
+        }
+
+        public Program()
+        {
+
+        }
+
+        public void go()
+        {
+            Console.WriteLine("Farkler!");
+            Console.WriteLine(ExpectedValueCalc.EV(6, 0));
+            Console.WriteLine(ExpectedValueCalc.EV(2, 50));
+            Console.ReadLine();
+        }
+    }
+}
