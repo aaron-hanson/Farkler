5d6800a4750a45630f082fc81e259f05a31f4fb6
juanbond <archaeal@gmail.com>
2015-02-28 08:38:22 -0600

    evopening

--- Farkler/ExpectedValueCalc.cs
+++ Farkler/ExpectedValueCalc.cs
@@ -9,22 +9,56 @@ namespace Farkler
 {
     class ExpectedValueCalc
     {
-        public static Dictionary<string, double> EVCache = new Dictionary<string, double>();
+        public static int WinScore { get; set; }
+        public static int MinBank { get; set; }
+        public static int MinOpen { get; set; }
+        public static Dictionary<string, double> EVCache;
+        public static Dictionary<string, double> EVOpeningCache;
+
+        static ExpectedValueCalc()
+        {
+            WinScore = 10000;
+            MinBank = 300;
+            MinOpen = 500;
+            EVCache = new Dictionary<string, double>();
+            EVOpeningCache = new Dictionary<string, double>();
+        }
 
         public static double EV(int d, double p)
         {
-            if (p >= 10000) return p;
+            if (p >= WinScore) return p;
 
             double ev;
-            string key = string.Format("{0}:{1}", d, p);
+            string key = d + ":" + p;
             if (EVCache.TryGetValue(key, out ev)) return ev;
 
             ev = Dice.Permute[d]
                 .Average(x => Farkle.GenerateActions(x)
                     .Max(y => (double?)EV(y.DiceToRoll, p + y.ScoreToAdd)) ?? 0D);
 
-            ev = p < 300 ? ev : Math.Max(ev, p);
+            ev = p < MinBank ? ev : Math.Max(ev, p);
             EVCache.Add(key, ev);
+            Console.Write('.');
+            return ev;
+        }
+
+        public static double EVOpening(int d, double p)
+        {
+            if (p >= WinScore) return p;
+
+            double ev;
+            string key = d + ":" + p;
+            if (EVOpeningCache.TryGetValue(key, out ev)) return ev;
+
+            ev = Dice.Permute[d]
+                .Average(x => Farkle.GenerateActions(x)
+                    .Max(y => (double?)(
+                        (p + y.ScoreToAdd) >= MinOpen
+                            ? p + y.ScoreToAdd
+                            : EVOpening(y.DiceToRoll, p + y.ScoreToAdd))) ?? 0D);
+
+            ev = (p < MinBank || p < MinOpen) ? ev : Math.Max(ev, p);
+            EVOpeningCache.Add(key, ev);
             Console.Write('.');
             return ev;
         }
